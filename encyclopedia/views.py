76697af781c9a8e73d6ac855d593ad17fea65331
PieterO99 <pieterout99@gmail.com>
2021-11-29 11:35:26 +0100

    added entries and clickable index

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    if title in util.list_entries():
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "contents": util.get_entry(title)
+        })
+    else: #give error message
+        return render(request, "encyclopedia/entry.html", {
+            "title": title.capitalize()
+        })
