f9817a7d5421b9c2e6c43b6b558b54c43125e84a
PieterO99 <pieterout99@gmail.com>
2021-11-30 11:35:14 +0100

    markdown to html conversion

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/new_page", views.new_page, name="new_page"),
     path("wiki/search", views.search, name="search"),
     path("wiki/<str:title>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from markdown2 import markdown
 from . import util
 
 
@@ -14,14 +14,47 @@ def entry(request, title):
         if title.upper() == entry.upper():
             return render(request, "encyclopedia/entry.html", {
             "title": entry,
-            "contents": util.get_entry(entry) })
+            "contents": markdown(util.get_entry(entry))} )
 
     # else give error message
     return render(request, "encyclopedia/error.html", {
-        "page": f"wiki/{title}"
+        "page": f"wiki/{title}",
+        "error": "page could not be found"
     })
 
 def search(request):
     title = request.GET["q"]
-    return entry(request, title)
+    # if title corresponds to an existing page, render it
+    if title.upper() in map(lambda x: x.upper(), util.list_entries()):
+        return entry(request, title)
+    else:
+        return results(request, title)
+
+def results(request, title):
+    results = []
+    for entry in util.list_entries():
+        if title.upper() in entry.upper():
+            results.append(entry)
+
+    return render(request, "encyclopedia/results.html", {
+        "title": title,
+        "results": results
+    })
+    
+def new_page(request):
+    try:
+        title = request.GET["title"]
+        print(util.list_entries(), "hoi")
+        if title.upper() in map(lambda x: x.upper(), util.list_entries()):
+            return render(request, "encyclopedia/error.html", {
+                "page": f"wiki/{title}",
+                "error": "page already exists"
+                })
+        content = request.GET["new_content"]
+        print(content)
+        util.save_entry(title, content)
+        return entry(request, title)
+    except:
+        return render (request, "encyclopedia/new_page.html", {
+    })
     
